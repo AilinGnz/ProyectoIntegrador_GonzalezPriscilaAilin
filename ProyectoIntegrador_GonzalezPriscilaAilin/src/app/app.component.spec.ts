c284055d6a5a9205e1762f2b7abc2eb473eb1b02
Priscila Ailin Gonzalez <ailinpriscila94@gmail.com>
2023-02-22 15:23:19 -0300

    Se instala NodeJs al proyecto

---
+++ ProyectoIntegrador_GonzalezPriscilaAilin/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ProyectoIntegrador_GonzalezPriscilaAilin'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ProyectoIntegrador_GonzalezPriscilaAilin');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProyectoIntegrador_GonzalezPriscilaAilin app is running!');
+  });
+});

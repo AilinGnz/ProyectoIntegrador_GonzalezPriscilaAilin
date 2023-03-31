021a79a52a1df93a07d9c0ce706c5f364c45f829
Priscila Ailin Gonzalez <ailinpriscila94@gmail.com>
2023-03-30 22:31:08 -0300

    auth controller
    avances en auth controller. seguir con los errores actuales.

---
+++ ProyectoIntegrador_GonzalezPriscilaAilin/BackEnd/src/main/java/com/porfoliognz/ailin/gnz/Security/Controller/Mensaje.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.porfoliognz.ailin.gnz.Security.Controller;
+
+/**
+ *
+ * @author ailin
+ */
+public class Mensaje {
+    private String mensaje;
+    
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}

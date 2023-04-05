8d8c9f75534e27d0c3568567df0bcb13e909ae47
Priscila Ailin Gonzalez <ailinpriscila94@gmail.com>
2023-04-05 16:59:21 -0300

    login components  - arreglar this.

--- ProyectoIntegrador_GonzalezPriscilaAilin/FrontEnd/src/app/service/token.service.ts
+++ ProyectoIntegrador_GonzalezPriscilaAilin/FrontEnd/src/app/service/token.service.ts
@@ -22,6 +22,32 @@ export class TokenService {
   }
 
   public setUserName(userName: string): void {
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, userName);
+  }
 
+  public getUser(): string {
+    return sessionStorage.getItem(USERNAME_KEY)!;
+  }
+
+  public setAuthorities(authorities: string[]): void {
+    window.sessionStorage.removeItem(AUTHORITIES_KEY);
+    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): String[] {
+    this.roles = [];
+    if (sessionStorage.getItem(AUTHORITIES_KEY)!) {
+      JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority: any) => {
+        this.roles.push(authority.authority);
+      });
+    }
+    return this.roles;
+  }
+
+  public logOut(): void {
+    window.sessionStorage.clear();
   }
 }
+
+
